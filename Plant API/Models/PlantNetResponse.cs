374702817742e969aac939915a72d73e28adfbf0
UA-Malgerus <mal8gerus@gmail.com>
2025-06-04 11:02:27 +0300

    Temporary fix of  unhandled exception in Plantcontroller. Removed required fields from PlantNetResponce

--- Plant API/Models/PlantNetResponse.cs
+++ Plant API/Models/PlantNetResponse.cs
@@ -4,20 +4,20 @@ namespace Plant_API.Models;
 
 public class PlantNetResponse
 {
-    [JsonProperty("results")] public required PlantNetResult[] Results { get; set; }
+    [JsonProperty("results")] public PlantNetResult[]? Results { get; set; }
 }
 
 public class PlantNetResult
-{
+{   
     [JsonProperty("score")] public double Score { get; set; }
 
-    [JsonProperty("species")] public required PlantSpecies Species { get; set; }
+    [JsonProperty("species")] public PlantSpecies? Species { get; set; }
 }
 
 public class PlantSpecies
 {
     [JsonProperty("scientificNameWithoutAuthor")]
-    public required string ScientificNameWithoutAuthor { get; set; }
+    public string? ScientificNameWithoutAuthor { get; set; }
 
     [JsonProperty("commonNames")] public string[] CommonNames { get; set; } = Array.Empty<string>();
 }
